71d757722cba5ba61407c1efa517e352acdbc404
dhanilkumart <dhanilkumart@gmail.com>
2024-04-08 15:25:29 +0530

    Add files via upload

---
+++ menuData.js
@@ -0,0 +1,46 @@
+export const menu = [
+  {
+    "name": "Leads",
+    "id": "leads",
+    "link": "#tiny.url/ls/liveapps.view/leadsv166139e42bc28e",
+    "icon": "",
+    "class": "menu-item",
+    "image": "",
+    "title":"",
+    "html": ""
+  },
+  {
+    "name": "Rewards",
+    "id": "rewards",
+    "link": "#tiny.url/ls/liveapps.view/rewardsv166139dc995edd",
+    "icon": "",
+    "class": "menu-item",
+    "image": "",
+    "title":"",
+    "html": "",
+    "level1": [
+      {
+        "name": "Revenue",
+        "id": "revenue",
+        "link": "#tiny.url/ls/liveapps.view/revenuee166139dfbccd85",
+        "icon": "",
+        "class": "submenu-item",
+        "image": "",
+        "title":"",
+        "html": "",
+        "level2": [
+          {
+            "name": "CRM",
+            "id": "crm",
+            "link": "#tiny.url/ls/liveapps.view/crmf166139e107e794",
+            "icon": "",
+            "class": "submenu-item",
+            "image": "",
+            "title":"",
+            "html": ""
+          },
+        ]
+      }
+    ]
+  },
+]
